8f718b73cc3cc93dc7c7f304177673bfd03e2f9b
Luis Alejandro Giraldo Bolaños <luis.giraldo3@utp.edu.co>
2025-11-24 20:04:16 -0500

    correción de backpropagation

--- neuronalNet.c
+++ neuronalNet.c
@@ -23,18 +23,18 @@ typedef struct{
 static void __w(float *w, unsigned int n){
     for (unsigned int l = 0; l < n; l++) {
         w[l] = ((float)rand()/ RAND_MAX)-0.5f;
-        //printf("w[%i] = %f\n", l, w[l]);
+        printf("[%i] = %f\n", l, w[l]);
     }
 }
 
 float neuron(float *x, 
-                    float w, 
+                    float *w, 
                     float *b, 
                     unsigned int n,
                     float (*activacion)(float)){
     float result = 0;
     while(n--){
-        result += activacion(x[n] * w + b[n]);
+        result += activacion(x[n] * w[n] + b[n]);
     }
     return result;
 }
@@ -48,12 +48,12 @@ Model *fit(float **x,
                 float (*devError)(float, float),
                 float InitB,
                 unsigned int length){
-    Model *model = (Model *) malloc(sizeof(Model));
     Layer layer;
     float ***w = (float ***) malloc(sizeof(float **)*(length-1));
     float ***deltas = (float ***) malloc(sizeof(float **)*(length-1));
     float **neu = (float **) malloc(sizeof(float *)*length);
     float **b = (float **) malloc(sizeof(float *)*length);
+    printf("configuración inicial\n");
     for(int i = 0; i < length; i++){
         layer = sequential[i];
         neu[i] = (float *) malloc(sizeof(float)*layer.neurons);
@@ -66,23 +66,32 @@ Model *fit(float **x,
                 w[i][j] = (float *) malloc(sizeof(float)*(sequential[i+1].neurons));
                 deltas[i][j] = (float *) malloc(sizeof(float)*(sequential[i+1].neurons));
                 //printf("%i %i\n", i, j);
+                printf("w[%i][%i]", i, j);
                 __w(w[i][j], sequential[i+1].neurons);
             }
         }
     }
+    printf("entrenamiento:\n");
     while(epochs--){
         for(int a = 0; a < n; a++){
+            printf("data  %i\n", a);
+            printf("capa  0\n");
             for(unsigned int j = 0; j < sequential[0].neurons; j++){
                 neu[0][j] = x[a][j];
+                printf("neu[%i][%i] = %f\n", 0, j, neu[0][j]);
             }
             for(unsigned int i = 1; i < length; i++){
+                printf("capa  %i\n", i);
                 layer = sequential[i];
                 //printf("sequential[%i] = %i, sequential[%i] = %i\n", i, layer.neurons, i-1, sequential[i-1].neurons);
                 for(unsigned int j = 0; j < layer.neurons; j++){
+                    float *sw = (float *)malloc(sizeof(float)*sequential[i-1].neurons);
                     for(unsigned int k = 0; k < sequential[i-1].neurons; k++){
-                        //printf("indices: %i %i %i", i-1, k, j);
-                        neu[i][j] = neuron(neu[i-1], w[i-1][k][j], b[i-1], sequential[i-1].neurons, sequential[i-1].activacion);
+                        sw[k] = w[i-1][k][j];
                     }
+                    neu[i][j] = neuron(neu[i-1], sw, b[i-1], sequential[i-1].neurons, sequential[i-1].activacion);
+                    free(sw);
+                    printf("neu[%i][%i] = %f\n", i, j, neu[i][j]);
                 }
                 //printf("\n");
             }
@@ -93,31 +102,31 @@ Model *fit(float **x,
             float delta = 1;
             layer = sequential[length - 1];
             Layer before = sequential[length-2];
+            
             for(int j = 0; j < before.neurons; j++){
-                for(int k = 0; k < layer.neurons; k++){
-                    delta = neuron(neu[length-2], w[length-2][j][k], b[length-2], before.neurons, before.derivada);
-                    deltas[length-2][j][k] = devError(neu[length-1][k], y[a][k]) * delta;
-                    delta = learning_rate * deltas[length-2][j][k] * delta;
-                    w[length-2][j][k] -= delta*neu[length-2][j];
-                    b[length-2][j] -= delta;
+                float *sw = (float *)malloc(sizeof(float)*before.neurons);
+                for(unsigned int k = 0; k < before.neurons; k++){
+                    sw[k] = w[length-2][k][j];
                 }
+                delta = neuron(neu[length-2], sw, b[length-2], before.neurons, before.derivada);
             }
-            for(int i = length - 2;i > 0; i--){
-                layer = sequential[i];
-                before = sequential[i-1];
-                for(int j = 0; j < before.neurons; j++){
-                    for(int k = 0; k < layer.neurons; k++){
-                        delta = neuron(neu[i-1], w[i-1][j][k], b[i-1], before.neurons, before.derivada);
-                        //printf("1 deltas[%i][%i][%i] = %f\n", i-1, j, k, deltas[i-1][j][k]);
-                        deltas[i-1][j][k] = deltas[i][k][0] * delta;
-                        delta = learning_rate * deltas[i-1][j][k] * delta;
-                        w[i-1][j][k] -= delta*neu[i-1][j];
-                        b[i-1][j] -= delta;
-                    }
-                }
-            }
+    //         for(int i = length - 2;i > 0; i--){
+    //             layer = sequential[i];
+    //             before = sequential[i-1];
+    //             for(int j = 0; j < before.neurons; j++){
+    //                 for(int k = 0; k < layer.neurons; k++){
+    //                     delta = neuron(neu[i-1], w[i-1][j][k], b[i-1], before.neurons, before.derivada);
+    //                     //printf("1 deltas[%i][%i][%i] = %f\n", i-1, j, k, deltas[i-1][j][k]);
+    //                     deltas[i-1][j][k] = deltas[i][k][0] * delta;
+    //                     delta = learning_rate * deltas[i-1][j][k] * delta;
+    //                     w[i-1][j][k] -= delta*neu[i-1][j];
+    //                     b[i-1][j] -= delta;
+    //                 }
+    //             }
+    //         }
         }
     }
+    Model *model = (Model *) malloc(sizeof(Model));
     model->b = b;
     model->w = w;
     model->sequential = sequential;
@@ -128,34 +137,40 @@ Model *fit(float **x,
 
 int main(){
     srand(time(NULL));
-    int n = 2;
+    int n = 3;
     float **x = (float **) malloc(sizeof(float *) * 4);
-    x[0] = (float *) malloc(sizeof(float) * 1);
+    x[0] = (float *) malloc(sizeof(float) * 2);
     x[0][0] = 0;
-    x[1] = (float *) malloc(sizeof(float) * 1);
-    x[1][0] = 1;
-    x[2] = (float *) malloc(sizeof(float) * 1);
-    x[2][0] = 2;
-    x[3] = (float *) malloc(sizeof(float) * 1);
-    x[3][0] = 3;
+    x[0][1] = 0;
+    x[1] = (float *) malloc(sizeof(float) * 2);
+    x[1][0] = 0;
+    x[1][1] = 1;
+    x[2] = (float *) malloc(sizeof(float) * 2);
+    x[2][0] = 1;
+    x[2][1] = 0;
+    x[3] = (float *) malloc(sizeof(float) * 2);
+    x[3][0] = 1;
+    x[3][1] = 1;
     float **y = (float **) malloc(sizeof(float *) * 4);
     y[0] = (float *) malloc(sizeof(float) * 1);
-    y[0][0] = 1;
+    y[0][0] = 0;
     y[1] = (float *) malloc(sizeof(float) * 1);
-    y[1][0] = 3;
+    y[1][0] = 1;
     y[2] = (float *) malloc(sizeof(float) * 1);
-    y[2][0] = 5;
+    y[2][0] = 1;
     y[3] = (float *) malloc(sizeof(float) * 1);
-    y[3][0] = 7;
-    Layer inicio = {1, relu, devRelu, "inicio"};
+    y[3][0] = 0;
+    Layer inicio = {2, relu, devRelu, "inicio"};
+    Layer medio = {1, relu, devRelu, "medio"};
     Layer final = {1, relu, devRelu, "final"};
     Sequential se = (Sequential) malloc(sizeof(Layer)*n);
     se[0] = inicio;
-    se[1] = final;
+    se[1] = medio;
+    se[2] = final;
     Model m = *(fit(x, 
                     y, 
                     4, 
-                    900, 
+                    1, 
                     se, 
                     mse,
                     devMse,
